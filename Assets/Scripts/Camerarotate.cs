4665514710e4575850fc3be1651d058b846418d3
Tim Buhrs <tim.buhrs@gmail.com>
2020-04-08 14:59:58 +0200

    Add a test scene

---
+++ Assets/Scripts/Camerarotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerarotate : MonoBehaviour
+{
+
+    public float speed;
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
