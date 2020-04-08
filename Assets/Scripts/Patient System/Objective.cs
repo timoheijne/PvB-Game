de9e5568e80b5279126261ba68a87d524c85f410
Timo Heijne <me@timoheijne.nl>
2020-04-08 19:26:11 +0200

    Add basic patient system objects

---
+++ Assets/Scripts/Patient System/Objective.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "FILENAME", menuName = "Objects/Objective", order = 0)]
+public class Objective : ScriptableObject
+{
+    public string FriendlyName;
+    public string InternalName;
+    public bool Active = true;
+
+
+}
---
+++ Assets/Scripts/Patient System/PatientFile.cs
@@ -0,0 +1,14 @@
+﻿using Shared;
+using UnityEngine;
+
+[UnityEngine.CreateAssetMenu(fileName = "PATIENTFILE", menuName = "Objects/Patient File", order = 0)]
+public class PatientFile : UnityEngine.ScriptableObject
+{
+    public string PatientName;
+    public int Age;
+    public GameObject Prefab;
+    public Difficulty Difficulty  = Difficulty.Beginner;
+    public bool Active = true;
+    
+    public Objective[] Objectives;
+}
