64ff3e97f716d3f08668a8ff85e33f115aa4835c
Timo Heijne <me@timoheijne.nl>
2020-04-09 14:47:53 +0200

    Add patient system (Loading & Tracking)

---
+++ Assets/Scripts/Patient System/PatientSystem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PatientSystem : MonoBehaviour
+{
+    public static  PatientSystem Instance;
+    
+    public event Action<PatientFile> OnPatientDone;
+    public event Action<PatientFile> OnPatientChange;
+    public PatientFile ActivePatient => _activePatient;
+    
+    private PatientFile _activePatient;
+    private List<PatientFile> _patientFiles = new List<PatientFile>();
+
+    private void Awake()
+    {
+        if (PatientSystem.Instance == null)
+        {
+            PatientSystem.Instance = this;
+        }
+        else
+        {
+            Debug.LogError("PatientSystem already loaded.. Destroying");
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        _patientFiles = Resources.LoadAll<PatientFile>("Patient Files").ToList();
+    }
+    
+    public bool SetActivePatient(PatientFile patient)
+    {
+        if (patient.Active)
+        {
+            _activePatient = patient;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void MarkObjectiveDone(string objectiveName)
+    {
+        if (this.ActivePatient == null) return;
+        
+        this.ActivePatient.MarkObjectiveDone(objectiveName);
+        
+        if (this.ActivePatient.IsDone())
+        {
+            OnPatientDone?.Invoke(this.ActivePatient);
+        }
+    }
+    
+    
+}
