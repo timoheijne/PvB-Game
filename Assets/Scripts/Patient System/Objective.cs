fd0d4539f2bf4ed67dd13b3b71335b7a26c8d9a2
Timo Heijne <me@timoheijne.nl>
2020-04-09 17:14:14 +0200

    Remove default initialization

--- Assets/Scripts/Patient System/Objective.cs
+++ Assets/Scripts/Patient System/Objective.cs
@@ -3,9 +3,9 @@
 [CreateAssetMenu(fileName = "FILENAME", menuName = "Objects/Objective", order = 0)]
 public class Objective : ScriptableObject
 {
-    public string FriendlyName;
+    public string FriendlyName ;
     public string InternalName;
     public bool Active = true;
 
-    public bool IsDone = false;
+    public bool IsDone;
 }
