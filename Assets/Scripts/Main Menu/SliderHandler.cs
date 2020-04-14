880866bb317f9c7f85c196f4a26610135ed3977d
MarrowOfAlbion <jerry.sam@ziggo.nl>
2020-04-14 13:12:03 +0200

    Add volume sliders functionality it will add an AudioManager script

--- Assets/Scripts/Main Menu/SliderHandler.cs
+++ Assets/Scripts/Main Menu/SliderHandler.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SliderHandler : MonoBehaviour
 {
+    public Slider[] volumeSliders;
+
+    private void Start() 
+    {
+        volumeSliders[0].value = AudioManager.instance.masterVolumePercent;
+        volumeSliders[1].value = AudioManager.instance.musicVolumePercent;
+        volumeSliders[2].value = AudioManager.instance.sfxVolumePercent;
+    }
+
     public void SetMasterVolume(float value) 
     {
-
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
     }
 
     public void SetMusicVolume(float value) 
     {
-
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
     }
 
     public void SetSfxVolume(float value) 
     {
-
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Sfx);
     }
 }
