fb7b5b5ff5643c4ea5eb5e6f3891eb2b1de62f40
Timo Heijne <me@timoheijne.nl>
2020-04-15 18:49:44 +0200

    Add Object Interactor

---
+++ Assets/Scripts/Interactable Object System/ObjectInteractor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class ObjectInteractor : MonoBehaviour
+{
+        [SerializeField]
+        private float _maxDistance = .5f;
+
+        public bool InteractWithObject()
+        {
+                if(PatientSystem.Instance == null) throw new Exception("Patient System Not Found");
+                GameObject _gameObject = FindObject();
+                
+                if (_gameObject == null) return false;
+                InteractableObject _interactableObject = _gameObject.GetComponent<InteractableObject>();
+                return _interactableObject != null && PatientSystem.Instance.MarkObjectiveDone(_interactableObject.ObjectiveName);;
+        }
+
+        private GameObject FindObject()
+        {
+                bool _hitDetect = Physics.BoxCast(transform.position, transform.localScale, transform.forward, out RaycastHit _hitInfo, Quaternion.identity, _maxDistance);
+                return (_hitDetect) ?  _hitInfo.transform.gameObject : null;
+        }
+        
+        void OnDrawGizmos()
+        {
+                Gizmos.color = Color.red;
+
+                Gizmos.DrawRay(transform.position, transform.forward * _maxDistance * 2);
+                //Draw a cube at the maximum distance
+                Gizmos.DrawWireCube(transform.position + transform.forward * _maxDistance * 2, transform.localScale);
+        }
+}
