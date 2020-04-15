da8b895788252d6f2016e1de54a09d3172226427
Timo Heijne <me@timoheijne.nl>
2020-04-15 18:49:26 +0200

    Add Interactable Object Reference Script

---
+++ Assets/Scripts/Interactable Object System/InteractableObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string ObjectiveName => _objectiveName;
+
+    [SerializeField]
+    private string _objectiveName;
+    private Collider _collider;
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider>();
+
+        if (_collider == null || _collider.enabled == false)
+        {
+            Debug.LogError("Interactable object does not have collider");
+        }
+    }
+    
+    
+}
