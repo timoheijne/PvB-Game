e12ad42052b247cc08708a4c5c64af0ed702c814
Timo Heijne <me@timoheijne.nl>
2020-04-23 14:25:40 +0200

    Add more level object options

--- Assets/Scripts/Level System/LevelObject.cs
+++ Assets/Scripts/Level System/LevelObject.cs
@@ -4,6 +4,8 @@ public class LevelObject : UnityEngine.ScriptableObject
         public string LevelName;
         public Shared.Difficulty Difficulty;
         public string SceneName;
-
+        public bool IsEnabled = true;
+        public string NextLevel;
+        
         public PatientFile[] PatientFiles;
 }
