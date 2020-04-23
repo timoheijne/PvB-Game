f4135de218e00e7812e5987cf9c42d01412a6952
Timo Heijne <me@timoheijne.nl>
2020-04-23 14:12:04 +0200

    Add basic level system

---
+++ Assets/Scripts/Level System/PatientSystemHook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class PatientSystemHook : MonoBehaviour
+{
+    private LevelSystem _levelSystem;
+    private void Start()
+    {
+        if(PatientSystem.Instance == null)
+        {
+            throw new ArgumentNullException("PatientSystem");
+        }
+
+        _levelSystem = GetComponent<LevelSystem>();
+        if(_levelSystem == null) 
+        {
+            throw new ArgumentNullException("LevelSystem");
+        }
+        
+        
+        PatientSystem.Instance.OnPatientDone += OnPatientDone;
+    }
+
+    private void OnPatientDone(PatientFile obj)
+    {
+        throw new NotImplementedException();
+    }
+}
