6a0f9a916f9c3c1e25bbe283148ab1101fd839cc
coen <cavandiepen@gmail.com>
2020-05-02 13:53:09 +0200

    Remove unnecessary functions from Head.cs

--- Assets/Scripts/ScriptingSystem/Nodes/Head.cs
+++ Assets/Scripts/ScriptingSystem/Nodes/Head.cs
@@ -4,27 +4,5 @@ using UnityEngine;
 
 public class Head : MonoBehaviour
 {
-    private int currentNode;
-
-    private Node nextNode;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Next()
-    {
-        if (currentNode < 1)
-        {
-            currentNode++;
-        }
-    }
+    private Node self;
 }
