2d54c9399306673a57e8c67aed63fef9f3df9434
Timo Heijne <me@timoheijne.nl>
2020-05-07 21:39:37 +0200

    Add IEquatable to TutorialSection script

--- Assets/Scripts/Tutorial System/TutorialObject.cs
+++ Assets/Scripts/Tutorial System/TutorialObject.cs
@@ -12,13 +12,23 @@ public class TutorialObject : ScriptableObject
     public bool isEnabled = true;
 
     [Serializable]
-    public struct TutorialSection
+    public struct TutorialSection : IEquatable<TutorialSection>
     {
         public string Name;
         public string ObjectReferenceID;
         public Shared.Position PanelPosition;
         public bool DoctorOnLeft;
         public string Body;
+
+        public bool Equals(TutorialSection other)
+        {
+            return Name == other.Name && ObjectReferenceID == other.ObjectReferenceID && PanelPosition == other.PanelPosition && DoctorOnLeft == other.DoctorOnLeft && Body == other.Body;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is TutorialSection other && Equals(other);
+        }
     }
 
     public TutorialSection[] TutorialSections;
