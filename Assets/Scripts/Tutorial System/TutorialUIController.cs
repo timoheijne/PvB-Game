f29b8d948b72c093ccd0f61f37b7abed62f877b6
Timo Heijne <me@timoheijne.nl>
2020-05-07 23:44:02 +0200

    Add tutorial UI controls and display

--- Assets/Scripts/Tutorial System/TutorialUIController.cs
+++ Assets/Scripts/Tutorial System/TutorialUIController.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 public class TutorialUIController : MonoBehaviour
 {
     [SerializeField]
     private Transform _objectHighlight;
+
+    [SerializeField]
+    private GameObject _tutorialWindow;
+    
+    [SerializeField]
+    private GameObject _doctorObject;
+    
+    [SerializeField]
+    private GameObject _doctorLeftWaypoint;
+    
+    [SerializeField]
+    private GameObject _doctorRightWaypoint;
+    
+    [SerializeField]
+    private GameObject _nextButton;
+    
+    [SerializeField]
+    private GameObject _prevButton;
+    
+    [SerializeField]
+    private TextMeshProUGUI _textObject;
+
+    private int _activeSection;
     
     private void Start()
     {
@@ -15,12 +39,93 @@ public class TutorialUIController : MonoBehaviour
     private void OnDeactivate()
     {
         // TODO: Clean UI and hide object highlighters
-        throw new NotImplementedException();
+        _tutorialWindow.SetActive(false);
+        _doctorObject.SetActive(false);
+        _objectHighlight.gameObject.SetActive(false);
     }
 
     private void OnActive(TutorialObject obj)
     {
-        throw new NotImplementedException();
+        _activeSection = 0;
+        _tutorialWindow.SetActive(true);
+        _doctorObject.SetActive(true);
+        
+        UpdateSection();
+    }
+
+    private void UpdateSection()
+    {  
+        TutorialObject _activeTutorial = TutorialSystem.Instance.ActiveTutorial;
+        if (_activeTutorial.TutorialSections.Length > _activeSection)
+        {
+            TutorialObject.TutorialSection _section = _activeTutorial.TutorialSections[_activeSection];
+            
+            // Within bounds
+            _prevButton.SetActive(_activeSection != 0);
+            _nextButton.SetActive(_activeSection < _activeTutorial.TutorialSections.Length - 1);
+
+            _textObject.text = _section.Body;
+            
+            Vector3 doctorPosition = _doctorObject.transform.position;
+            Vector3 doctorScale = _doctorObject.transform.localScale;
+            
+            if (_section.DoctorOnLeft)
+            {
+                doctorScale.x = -1;
+                _doctorObject.transform.position = _doctorLeftWaypoint.transform.position;
+            }
+            else
+            {
+                _doctorObject.transform.position = _doctorRightWaypoint.transform.position;
+                doctorScale.x = 1;
+            }
+            
+            _doctorObject.transform.localScale = doctorScale;
+            
+            // TODO: SET HIGHLIGHT
+            if (_section.ObjectReferenceID != String.Empty)
+            {
+                HighlightObject(_section.ObjectReferenceID);
+            }
+            else
+            {
+                _objectHighlight.gameObject.SetActive(false);
+            }
+        } 
+        else if (_activeTutorial.TutorialSections.Length == 0)
+        {
+            Debug.LogError("Tutorial has no sections... Closing");
+            TutorialSystem.Instance.DeactivateTutorial();
+        }
+        else
+        {
+            throw new IndexOutOfRangeException();
+        }
+    }
+
+    public void NextSection()
+    {
+        TutorialObject _activeTutorial = TutorialSystem.Instance.ActiveTutorial;
+        _activeSection += 1;
+        if (_activeTutorial.TutorialSections.Length <= _activeSection)
+        {
+            TutorialSystem.Instance.DeactivateTutorial();
+        }
+        else
+        {
+            UpdateSection();
+        }
+    }
+
+    public void PreviousSection()
+    {
+        _activeSection -= 1;
+        if (_activeSection < 0)
+        {
+            _activeSection = 0;
+        }
+        
+        UpdateSection();
     }
 
     private void HighlightObject(string ObjectReference)
