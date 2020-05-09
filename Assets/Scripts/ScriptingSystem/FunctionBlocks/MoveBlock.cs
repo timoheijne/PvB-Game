cf44e4ea33c2b64d23534331867ab8adad784594
coen <cavandiepen@gmail.com>
2020-05-09 13:13:14 +0200

    Add MoveBlock.cs

---
+++ Assets/Scripts/ScriptingSystem/FunctionBlocks/MoveBlock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : FunctionBlock
+{
+    [SerializeField] private Movement movement;
+    
+    public override void Act()
+    {
+        if (movement.CanMoveForward())
+        {
+            movement.Forward();
+        }
+    }
+}
