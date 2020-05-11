f4cb8a79a6635e7c1e95d86638442c340a144d9b
Timo Heijne <me@timoheijne.nl>
2020-05-11 19:38:18 +0200

    Add first development level

--- Assets/Scripts/ScriptingSystem/FunctionBlocks/MoveBlock.cs
+++ Assets/Scripts/ScriptingSystem/FunctionBlocks/MoveBlock.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveBlock : FunctionBlock
 {
     [SerializeField] private Movement movement;
-    
+
+    private void Start()
+    {
+        // TODO: Refactor to not need to lookup object, This needs to be set dynamically via the visual scripting UI
+        movement = GameObject.FindWithTag("PawnObject").GetComponent<Movement>();
+    }
+
     public override IEnumerator Act()
     {
         if (movement.CanMoveForward())
