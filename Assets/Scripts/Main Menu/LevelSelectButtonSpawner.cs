73729dd8761d45e5e66d5d0f32f783d86b7f76a5
coen <cavandiepen@gmail.com>
2020-05-12 13:01:18 +0200

    Add a function to create level select buttons

---
+++ Assets/Scripts/Main Menu/LevelSelectButtonSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectButtonSpawner : MonoBehaviour
+{
+    LevelSystem levelSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelSystem = FindObjectOfType<LevelSystem>();
+    }
+
+
+    public GameObject CreateButton(LevelObject _level, Transform parent)
+    {
+        GameObject button = new GameObject();
+        button.transform.parent = parent;
+        RectTransform rect = button.AddComponent<RectTransform>();
+        button.AddComponent<Button>().onClick.AddListener(() => levelSystem.ChangeLevel(_level));
+        return button;
+    }
+}
