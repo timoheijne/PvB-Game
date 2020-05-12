8a6307a692e31f920266d7d3f581fae15d07e6cb
Timo Heijne <me@timoheijne.nl>
2020-05-13 01:57:59 +0200

    Add clamping of movement position

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
@@ -5,7 +6,15 @@ public class CameraMovement : MonoBehaviour
         [SerializeField]
         private float _dragSpeed = 2;
 
+        [SerializeField]
+        private Vector3 _minCamp;
+        
+        [SerializeField]
+        private Vector3 _maxClamp;
+        
         private Vector3 dragOrigin;
+        
+        
         
         void Update()
         {
@@ -18,9 +27,26 @@ public class CameraMovement : MonoBehaviour
                 if (!Input.GetMouseButton(0)) return;
 
                 Vector3 pos = Input.mousePosition - dragOrigin;
-                Debug.Log(pos);
                 
                 transform.position += new Vector3(pos.y * _dragSpeed * Time.deltaTime, 0, -pos.x * _dragSpeed * Time.deltaTime);
                 dragOrigin = Input.mousePosition;
+
+                ClampMovement();
+        }
+
+        private void ClampMovement()
+        {
+                Vector3 _newPos = transform.position;
+                _newPos.x = Mathf.Clamp(_newPos.x, _minCamp.x, _maxClamp.x);
+                _newPos.z = Mathf.Clamp(_newPos.z, _minCamp.z, _maxClamp.z);
+
+                transform.position = _newPos;
+        }
+
+        private void OnDrawGizmos()
+        {
+                Gizmos.color = Color.red;
+                Vector3 midPoint = new Vector3((_minCamp.x+_maxClamp.x)/2.0f,(_minCamp.y+_maxClamp.y)/2.0f,(_minCamp.z+_maxClamp.z)/2.0f);
+                Gizmos.DrawWireCube(midPoint, new Vector3(_maxClamp.x - _minCamp.x, 1, _maxClamp.z - _minCamp.z));
         }
 }
