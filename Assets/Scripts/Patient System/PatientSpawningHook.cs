a2fd7b8e1082a5c18c69fbf11d6c2fe86e03bd06
Timo Heijne <me@timoheijne.nl>
2020-05-12 18:31:49 +0200

    Refactor Patient data tracker so the patient file is set dynamically

--- Assets/Scripts/Patient System/PatientSpawningHook.cs
+++ Assets/Scripts/Patient System/PatientSpawningHook.cs
@@ -32,6 +32,10 @@ public class PatientSpawningHook : MonoBehaviour
 
         GameObject _patient = Instantiate(obj.Prefab);
         Patient _data = _patient.GetComponent<Patient>();
+        
+        _data.PatientFile = obj;
+        _data.PatientFile.GameObject = _patient;
+        
         if (_data == null)
         {
             throw new ArgumentNullException("Patient Data");
