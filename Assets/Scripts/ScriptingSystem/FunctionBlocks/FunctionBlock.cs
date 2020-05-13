fb6e85c8287a3a5ea41dbf57151e37edcf256f6e
Timo Heijne <me@timoheijne.nl>
2020-05-13 13:23:27 +0200

    Add unit of time to tooltip

--- Assets/Scripts/ScriptingSystem/FunctionBlocks/FunctionBlock.cs
+++ Assets/Scripts/ScriptingSystem/FunctionBlocks/FunctionBlock.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class FunctionBlock : MonoBehaviour
 {
-    [SerializeField, Tooltip("Time this block should wait before preceding to the next block, This is purely cosmetic and is not for solving a race condiction")] 
+    [SerializeField, Tooltip("Seconds this block should wait before preceding to the next block, This is purely cosmetic and is not for solving a race condiction")] 
     protected float _timeout;
     
     public abstract IEnumerator Act();
