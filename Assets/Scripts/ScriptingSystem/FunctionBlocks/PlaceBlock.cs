c793c471d4b7db729edf796eca1f91a48a1ea2af
Timo Heijne <me@timoheijne.nl>
2020-05-13 21:18:03 +0200

    Refactor PlacedownBlock with ActorObject

--- Assets/Scripts/ScriptingSystem/FunctionBlocks/PlaceBlock.cs
+++ Assets/Scripts/ScriptingSystem/FunctionBlocks/PlaceBlock.cs
@@ -3,28 +3,25 @@ using UnityEngine;
 
 public class PlaceBlock : FunctionBlock
 {
-    [SerializeField] private ObjectInteractor _interactor;
-    [SerializeField] private CarryObject _carryObject;
+    private ActorObject _actorObject;
     
     private void Start()
     {
         // TODO: Refactor to not need to lookup object, This needs to be set dynamically via the visual scripting UI
-        GameObject _pawn = GameObject.FindWithTag("PawnObject");
-        _interactor = _pawn.GetComponent<ObjectInteractor>();
-        _carryObject = _pawn.GetComponent<CarryObject>();
+        _actorObject = GameObject.FindWithTag("PawnObject").GetComponent<ActorObject>();
     }
     
     public override IEnumerator Act()
     {
-        InteractableObject _interactableObject = _interactor.FindInteractableObject();
+        InteractableObject _interactableObject = _actorObject.ObjectInteractor.FindInteractableObject();
         if (_interactableObject == null)
         {
             yield break;
         }
 
-        if (_carryObject.IsCarrying())
+        if (_actorObject.CarryObject.IsCarrying())
         {
-            Patient _target = _carryObject.StopCarry().GetComponent<Patient>();
+            Patient _target = _actorObject.CarryObject.StopCarry().GetComponent<Patient>();
             if (_target != null)
             {
                 _interactableObject.PlacePatient(_target);
