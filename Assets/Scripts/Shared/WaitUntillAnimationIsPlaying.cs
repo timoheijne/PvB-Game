af57e880c2489b7c3d2c116936737c1314782e92
Timo Heijne <me@timoheijne.nl>
2020-05-14 12:45:39 +0200

    Set yield instructions to readonly

--- Assets/Scripts/Shared/WaitUntillAnimationIsPlaying.cs
+++ Assets/Scripts/Shared/WaitUntillAnimationIsPlaying.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class WaitUntillAnimationIsPlaying : CustomYieldInstruction
 {
-    private Animator _animator;
-    private int _layer;
-    private string _name;
+    private readonly Animator _animator;
+    private readonly int _layer;
+    private readonly string _name;
 
     public override bool keepWaiting => !_animator.GetCurrentAnimatorStateInfo(_layer).IsName(_name);
 
