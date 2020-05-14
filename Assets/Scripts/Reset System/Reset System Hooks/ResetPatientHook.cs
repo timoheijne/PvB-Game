96681d7a07c1451f0fa783c6f04d7e21bb17a12a
Timo Heijne <me@timoheijne.nl>
2020-05-14 17:12:28 +0200

    Refactor reset patient system hook to reset patient hook

---
+++ Assets/Scripts/Reset System/Reset System Hooks/ResetPatientHook.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+public class ResetPatientHook : ResetBehaviour
+{
+    private Patient _patient;
+    private InteractableObject _originalObject;
+
+    private void Start()
+    {
+        _patient = GetComponent<Patient>();
+
+        if (_patient == null)
+        {
+            Debug.LogError("Patient object not found on GameObject Destroying");
+            Destroy(this);
+        }
+        
+        InteractableObject[] _objects = GameManager.FindObjectsOfType<InteractableObject>();
+        foreach (InteractableObject _object in _objects)
+        {
+            if (!_object.HasPatient()) continue;
+            
+            _originalObject = _object;
+            return;
+        }
+    }
+
+    public override void InvokeReset()
+    {
+        InteractableObject[] _objects = GameManager.FindObjectsOfType<InteractableObject>();
+
+        foreach (InteractableObject _object in _objects)
+        {
+            _object.TakePatient();
+        }
+        
+        _patient.PatientFile.ResetObjectives();
+        _originalObject.PlacePatient(_patient);
+    }
+}
