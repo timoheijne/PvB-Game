9a0a45a0a2ecacb1d54f6e03e2a45d7f4b2e4adb
Timo Heijne <me@timoheijne.nl>
2020-05-14 21:50:26 +0200

    Fix compliancy clock script

--- Assets/Props/Tim map/Klok/Clock/Scripts/Clock.cs
+++ Assets/Props/Tim map/Klok/Clock/Scripts/Clock.cs
@@ -17,51 +17,51 @@ public class Clock : MonoBehaviour {
     public float clockSpeed = 1.0f;     // 1.0f = realtime, < 1.0f = slower, > 1.0f = faster
 
     //-- internal vars
-    float msecs=0;
+    private float msecs;
 
-void Start() 
-{
-	//-- set real time
-	if (realTime)
+	void Start() 
 	{
-		hour=System.DateTime.Now.Hour;
-		minutes=System.DateTime.Now.Minute;
-		seconds=System.DateTime.Now.Second;
+		//-- set real time
+		if (realTime)
+		{
+			hour=System.DateTime.Now.Hour;
+			minutes=System.DateTime.Now.Minute;
+			seconds=System.DateTime.Now.Second;
+		}
 	}
-}
 
-void Update() 
-{
-    //-- calculate time
-    msecs += Time.deltaTime * clockSpeed;
-    if(msecs >= 1.0f)
-    {
-        msecs -= 1.0f;
-        seconds++;
-        if(seconds >= 60)
-        {
-            seconds = 0;
-            minutes++;
-            if(minutes > 60)
-            {
-                minutes = 0;
-                hour++;
-                if(hour >= 24)
-                    hour = 0;
-            }
-        }
-    }
+	void Update() 
+	{
+	    //-- calculate time
+	    msecs += Time.deltaTime * clockSpeed;
+	    if(msecs >= 1.0f)
+	    {
+	        msecs -= 1.0f;
+	        seconds++;
+	        if(seconds >= 60)
+	        {
+	            seconds = 0;
+	            minutes++;
+	            if(minutes > 60)
+	            {
+	                minutes = 0;
+	                hour++;
+	                if(hour >= 24)
+	                    hour = 0;
+	            }
+	        }
+	    }
 
 
-    //-- calculate pointer angles
-    float rotationSeconds = (360.0f / 60.0f)  * seconds;
-    float rotationMinutes = (360.0f / 60.0f)  * minutes;
-    float rotationHours   = ((360.0f / 12.0f) * hour) + ((360.0f / (60.0f * 12.0f)) * minutes);
+	    //-- calculate pointer angles
+	    float rotationSeconds = (360.0f / 60.0f)  * seconds;
+	    float rotationMinutes = (360.0f / 60.0f)  * minutes;
+	    float rotationHours   = ((360.0f / 12.0f) * hour) + ((360.0f / (60.0f * 12.0f)) * minutes);
 
-    //-- draw pointers
-    pointerSeconds.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationSeconds);
-    pointerMinutes.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationMinutes);
-    pointerHours.transform.localEulerAngles   = new Vector3(0.0f, 0.0f, rotationHours);
+	    //-- draw pointers
+	    pointerSeconds.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationSeconds);
+	    pointerMinutes.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationMinutes);
+	    pointerHours.transform.localEulerAngles   = new Vector3(0.0f, 0.0f, rotationHours);
 
-}
+	}
 }
