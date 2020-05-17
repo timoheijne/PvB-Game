180388d2d006d9c946d6aa8745a68b759cb492d6
Timo Heijne <me@timoheijne.nl>
2020-05-17 14:34:43 +0200

    Add center to position enum

--- Assets/Scripts/Level Scripts/FirstLevel.cs
+++ Assets/Scripts/Level Scripts/FirstLevel.cs
@@ -1,6 +1,11 @@
 using System;
 using UnityEngine;
 
+
+// This script is for controlling the tutorials in the first level
+// Preferably this would've been made dynamically with scriptable objects and such
+// However, due to time limitations, This will be done the ugly but fast way.
+
 public class FirstLevel : MonoBehaviour
 {
     private void Start()
