280307a91c488e15ae57d3930fd2f79e94a8ed60
Timo Heijne <me@timoheijne.nl>
2020-05-19 19:13:53 +0200

    Add basic pause screen to level & toggle

---
+++ Assets/Scripts/Pause Screen/PauseScreen.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+    private GameObject _childPanel;
+
+    private void Start()
+    {
+        _childPanel = transform.GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleMenu();;
+        }
+    }
+
+    private void ToggleMenu()
+    {
+        _childPanel.SetActive(!_childPanel.activeInHierarchy);
+        
+        if (_childPanel.activeInHierarchy)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
