c00e6973fd71e34e5230c24ddddd30c956079aec
coen <cavandiepen@gmail.com>
2020-05-27 12:16:44 +0200

    Add bounds to DragUI.cs

--- Assets/DragWorkspace.cs
+++ Assets/DragWorkspace.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
-using System.Security.Cryptography;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine;
 
-public class DragUI : MonoBehaviour, IPointerDownHandler
+public class DragWorkspace : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField]
-    private RectTransform WorkspaceTransform;
+    private GameObject Workspace;
 
     private CameraMovement _cameraMovement;
     private Vector2 lastMousePosition;
@@ -21,16 +17,12 @@ public class DragUI : MonoBehaviour, IPointerDownHandler
         _cameraMovement = Camera.main.transform.parent.GetComponent<CameraMovement>();
     }
 
-    /// <summary>
-    /// This method will be called on the start of the mouse drag
-    /// </summary>
-    /// <param name="eventData">mouse pointer event data</param>
     public void OnPointerDown(PointerEventData eventData)
     {
         lastMousePosition = eventData.position;
         GetComponent<Node>()?.RemoveNode();
         isDragging = true;
-        
+
     }
 
     private void Update()
@@ -38,15 +30,15 @@ public class DragUI : MonoBehaviour, IPointerDownHandler
         if (!isDragging)
         {
             return;
-        }  
-        
+        }
+
         _cameraMovement?.SetFreeze(isDragging);
 
         if (isDragging && Input.GetMouseButtonUp(0))
         {
             ReleaseDrag();
         }
-        
+
         Vector2 currentMousePosition = Input.mousePosition;
         Vector2 diff = currentMousePosition - lastMousePosition;
         RectTransform rect = GetComponent<RectTransform>();
@@ -60,44 +52,20 @@ public class DragUI : MonoBehaviour, IPointerDownHandler
         }
         lastMousePosition = currentMousePosition;
     }
-    
+
     private void ReleaseDrag()
     {
         isDragging = false;
         _cameraMovement?.SetFreeze(false);
-
-        Node[] nodes = FindObjectsOfType<Node>();
-        foreach (Node node in nodes)
-        {
-            if (node.gameObject.tag.Equals("TrashCan") && GetComponent<Head>() == null && node.IsColliding(GetComponent<RectTransform>().position))
-            {
-                Destroy(gameObject);
-                return;
-            }
-            
-            if(node.gameObject != gameObject && node.IsColliding(GetComponent<RectTransform>().position) && GetComponent<Head>() == null && !node.gameObject.tag.Equals("TrashCan"))
-
-            {
-                Node _thisNode = GetComponent<Node>();
-                node.InsertNode(_thisNode, GetComponent<RectTransform>().rect.height + -_thisNode.UiOffset.y);
-                return;
-            }
-        }
     }
 
-    /// <summary>
-    /// This methods will check is the rect transform is inside the screen or not
-    /// </summary>
-    /// <param name="rectTransform">Rect Trasform</param>
-    /// <returns></returns>
     private bool IsRectTransformInsideSreen(RectTransform rectTransform)
     {
         bool isInside = false;
         Vector3[] corners = new Vector3[4];
         rectTransform.GetWorldCorners(corners);
         int visibleCorners = 0;
-        Rect rect = (WorkspaceTransform != null ? new Rect((1 - WorkspaceTransform.rect.width / 1920) * Screen.width, 100, WorkspaceTransform.rect.width / 1920 * Screen.width, Screen.height-100) : new Rect(0, 0, Screen.width, Screen.height));
-        
+        Rect rect = new Rect(0, 0, Screen.width, Screen.height);
         foreach (Vector3 corner in corners)
         {
             if (rect.Contains(corner))
